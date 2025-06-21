d0f8aa219826aaf5b8732b2d5b5e5fbcf90b66e3
Ganapati Kundapura <134807718+gkundapur@users.noreply.github.com>
2025-06-21 23:35:15 +0530

    Update BST_search_closest.cpp

--- BST_search_closest.cpp
+++ BST_search_closest.cpp
@@ -46,10 +46,37 @@ TreeNode *search_closest_iter(TreeNode *root, int k)
         return closest;
 }
 
+TreeNode *search_closest_rec(TreeNode *root, int k, int minDiff, TreeNode **closestNode)
+{
+        if (root == NULL)
+                return *closestNode;
+
+        int diff = abs(root->val - k);
+        if (diff < minDiff) {
+                minDiff = diff;
+                *closestNode = root;
+        }
+        
+        if (root->val == k)
+                return root;
+        else if (k < root->val)
+                return search_closest_rec(root->left, k, minDiff, closestNode);
+        else
+                return search_closest_rec(root->right, k, minDiff, closestNode);
+}
+
+TreeNode *search_closest(TreeNode *root, int k)
+{
+        TreeNode *closestNode = NULL;
+        return search_closest_rec(root, k, INT_MAX, &closestNode);
+}
+
+
 int
 main(void)
 {
         TreeNode *root = NULL;
+        TreeNode *result = NULL;
         int k;
 
         int keys[] = {10, 5, 15, 2, 7, 12, 20};
@@ -57,11 +84,20 @@ main(void)
                 root = insert(root, key);
 
         k = 16;
-        TreeNode *result = search_closest_iter(root, k);
+        // iterative search
+        result = search_closest_iter(root, k);
         if (result && result->val == k)
-                cout << "Exact match found: " << result->val << endl;
+                cout << "(Iter)Exact match found: " << result->val << endl;
         else
-                cout << "No exact match. Closest value: " << result->val << endl;
+                cout << "(Iter)No exact match. Closest value: " << result->val << endl;
+
+        // recursive search
+        k = 11;
+        result = search_closest(root, k);
+        if (result && result->val == k)
+                cout << "(rec)Exact match found: " << result->val << endl;
+        else
+                cout << "(rec)No exact match. Closest value: " << result->val << endl;
 
         return 0;
 }
